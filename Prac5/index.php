ade9032e627f313493595160e4716bb9d7dd8aa0
mari75a <sangeethlakshan0@gmail.com>
2025-06-15 20:57:06 +0530

    first commit

---
+++ Prac5/index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Practical 5</title>
+</head>
+
+<body>
+    <form action="formaction1.php" method="POST">
+        <p>Name: <input type="text" name="yourname" /><br />
+            E-mail: <input type="text" name="email" /></p>
+        <input type=submit value=submit>
+    </form>
+</body>
+
+</html>
